29566cffa45c97e42fa4dbe1064a3421e0418a7c
J <neirbonitsuj@gmail.com>
2025-10-05 08:38:12 -0700

    fix delete and upload 3

--- src/utils/songStorage.js
+++ src/utils/songStorage.js
@@ -1,4 +1,4 @@
-import { refreshTokens, getAuthHeader } from '../services/authService';
+import { refreshTokens, getAuthHeader, isAuthenticated } from '../services/authService';
 
 const API_URL = process.env.NODE_ENV === 'production' 
   ? '/.netlify/functions/songs' 
